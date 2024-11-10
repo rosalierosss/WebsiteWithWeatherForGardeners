f0bf188d237b8358bfea5e3639bf8fdfbb2dde10
KomaMix <ksabirov448@gmail.com>
2024-11-10 18:19:48 +0300

    Убрана боковая панель, а логика перенеслась в вкладку погода сверху

---
+++ WeatherForGardeners/Pages/Map.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WeatherForGardeners.Pages
+{
+    public class MapModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
