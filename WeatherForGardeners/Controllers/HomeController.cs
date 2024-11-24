a8672cf40c06efbce00ac6ec26ec2438fe6d4a81
KomaMix <ksabirov448@gmail.com>
2024-11-24 18:53:18 +0300

    Добавила карусель

--- WeatherForGardeners/Controllers/HomeController.cs
+++ WeatherForGardeners/Controllers/HomeController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WeatherForGardeners.Models;
 
 namespace WeatherForGardeners.Controllers
 {
     public class HomeController : Controller
     {
+        private List<Plant> plants;
+
+        public HomeController()
+        {
+            plants = new List<Plant>()
+            {
+                new Plant { Id = 1, Name = "Plant 1", Description = "Description 1", PhotoUrl = "/images/clover.jpg" },
+                new Plant { Id = 2, Name = "Plant 2", Description = "Description 2", PhotoUrl = "/images/hibiscus.jpeg" },
+            };
+
+            for (int i = 3; i < 16; i++)
+            {
+                plants.Add(new Plant
+                {
+                    Id = i,
+                    Name = $"Plant {i}",
+                    Description = $"Description {i}",
+                    PhotoUrl = $"/images/plant{i}.jpg"
+                });
+            }
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(plants);
         }
     }
 }
---
+++ WeatherForGardeners/Models/Plant.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherForGardeners.Models
+{
+    public class Plant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+}
