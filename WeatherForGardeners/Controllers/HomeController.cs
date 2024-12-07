74868f550b065c88d2ddde23c6806e818e4afc6a
KomaMix <ksabirov448@gmail.com>
2024-12-07 18:04:23 +0300

    added static data

--- WeatherForGardeners/Controllers/HomeController.cs
+++ WeatherForGardeners/Controllers/HomeController.cs
@@ -11,11 +11,13 @@ namespace WeatherForGardeners.Controllers
         {
             plants = new List<Plant>()
             {
-                new Plant { Id = 1, Name = "Plant 1", Description = "Description 1", PhotoUrl = "/images/clover.jpg" },
+                new Plant { Id = 1, Name = "Какое то растение", Description = "Это растение очень красивое. Проверка длинного описания. Проверка длинного описания. Проверка длинного описания. Проверка длинного описания. Проверка длинного описания. Проверка длинного описания. Проверка длинного описания.", PhotoUrl = "/images/clover.jpg" },
                 new Plant { Id = 2, Name = "Plant 2", Description = "Description 2", PhotoUrl = "/images/hibiscus.jpeg" },
+                new Plant { Id = 3, Name = "Plant 3", Description = "Description 3", PhotoUrl = "images/cat.jpeg" },
+                new Plant { Id = 4, Name = "Plant 4", Description = "Description 4", PhotoUrl = "/images/hibiscus.jpeg" },
             };
 
-            for (int i = 3; i < 16; i++)
+            for (int i = 5; i < 16; i++)
             {
                 plants.Add(new Plant
                 {
---
+++ WeatherForGardeners/wwwroot/js/carusel.js
@@ -0,0 +1,17 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector(".scrolling-content");
+    const items = Array.from(container.children);
+    const itemWidth = items[0].offsetWidth;
+    let currentIndex = 0;
+
+    function updateCarousel() {
+        container.style.transform = `translateX(-${currentIndex * itemWidth}px)`;
+
+        currentIndex++;
+        if (currentIndex >= items.length - 2) {
+            currentIndex = 0;
+        }
+    }
+
+    setInterval(updateCarousel, 5000);
+});
